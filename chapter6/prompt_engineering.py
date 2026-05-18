91c4c7dee326e22090cd9c616b2344793dc0b6cc
Yanbin Qiu <yabqiu@gmail.com>
2026-05-18 10:21:51 -0500

    leaning in progree

---
+++ chapter6/prompt_engineering.py
@@ -0,0 +1,27 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, GenerationConfig
+
+model_name = "google/gemma-4-E4B-it"
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    device_map="mps",
+    # trust_remote_code=True,
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+pip = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    return_full_text=False,
+    generation_config = GenerationConfig(
+        max_new_tokens=500,
+        do_sample=False,
+    )
+)
+
+messages = [
+    {"role": "user", "content": "Create a funny joke about chickens."}
+]
+
+output = pipe(messages)
+print(output[0]["generated_text"])
